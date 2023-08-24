e123f49c29138c4401eb4177a0eea6f3eef41c4e
xiaochuhan1997 <111564410+xiaochuhan1997@users.noreply.github.com>
2023-08-24 15:49:22 +0800

    Delete src directory

--- src/main/java/com/example/swagger/service/TestService.java
+++
@@ -1,9 +0,0 @@
-package com.example.swagger.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.swagger.entity.Test;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface TestService extends IService<Test> {
-}
