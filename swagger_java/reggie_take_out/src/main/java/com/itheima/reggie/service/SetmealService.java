1cedf1602707f58fc5f2289189fa169d77dd45ce
z224466 <782876280@qq.com>
2023-08-24 10:14:07 +0800

    初始化java代码
    初始化java代码

--- swagger_java/reggie_take_out/src/main/java/com/itheima/reggie/service/SetmealService.java
+++
@@ -1,7 +0,0 @@
-package com.itheima.reggie.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.itheima.reggie.entity.Setmeal;
-
-public interface SetmealService extends IService<Setmeal> {
-}
