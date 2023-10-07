34630dc087861adeb5d6aee19af0abe5ed9644bf
xiaochuhan <xiaochuhan@tfrunning.com.cn>
2023-10-07 15:36:32 +0800

    application.yml:追加swagger3相关配置，解决打开swagger页面后api-doc无法正确显示的问题。 pom.xml：修改依赖的版本，追加swagger3配置，解决依赖版本冲突问题 删除SwaggerConfig.java，移除swagger2的配置 追加Swagger3Config.java SwaggerApplication.java：追加注解以适配Swagger3 WebMvcConfig.java:解决Swagger页面静态资源无法访问的问题

--- swagger_java/src/main/java/com/example/swagger/SwaggerApplication.java
+++ swagger_java/src/main/java/com/example/swagger/SwaggerApplication.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @Slf4j
 @SpringBootApplication
 @ServletComponentScan
+@EnableOpenApi
 public class SwaggerApplication {
 
     public static void main(String[] args) {
